377ef2aca4104ba4949eee8f0a60894ce0d9befc
Jorge Diaz <jorgediazl1998@gmail.com>
2025-07-08 23:38:37 -0400

    Fixed querys

--- src/models/query.js
+++ src/models/query.js
@@ -1,7 +1,14 @@
 const { pool } = require("../database/connection");
 
 const getAllProducts = async () => {
-  const query = "SELECT * FROM productos";
+  const query = `
+  SELECT p.id, p.descripcion, p.descripcionDetallada, p.precio_venta, p.stock_actual, p.url_fotografia, cp.descripcion as categoria, json_build_object('id', u.id, 'name', u.nombre_completo) as user 
+  FROM productos p 
+  inner join categoriaProductos cp 
+  on p.id_categoria = cp.id 
+  inner join usuarios u 
+  on p.id_usuario = u.id
+  `;
 
   const { rows: products } = await pool.query(query);
 
@@ -18,6 +25,7 @@ const postNewProduct = async (payload) => {
     url_fotografia,
     estatus,
     id_categoria,
+    id_usuario,
   } = payload;
 
   const values = [
@@ -29,10 +37,11 @@ const postNewProduct = async (payload) => {
     url_fotografia,
     estatus,
     id_categoria,
+    id_usuario,
   ];
 
   const query =
-    "INSERT INTO productos VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8)";
+    "INSERT INTO productos VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9)";
 
   await pool.query(query, values);
 };
@@ -47,6 +56,7 @@ const putProduct = async (payload, id) => {
     url_fotografia,
     estatus,
     id_categoria,
+    id_usuario,
   } = payload;
 
   const values = [
@@ -58,11 +68,15 @@ const putProduct = async (payload, id) => {
     url_fotografia,
     estatus,
     id_categoria,
+    id_usuario,
     id,
   ];
 
-  const query =
-    "UPDATE productos SET sku = $1 ,descripcion = $2 ,descripcionDetallada = $3 ,precio_venta = $4 ,stock_actual = $5 ,url_fotografia = $6 ,estatus = $7 ,id_categoria = $8 WHERE id = $9";
+  const query = `
+  UPDATE productos 
+  SET sku = $1 ,descripcion = $2 ,descripcionDetallada = $3 ,precio_venta = $4 ,stock_actual = $5 ,url_fotografia = $6 ,estatus = $7 ,id_categoria = $8, id_usuario = $9 
+  WHERE id = $10
+  `;
 
   const { rowCount } = await pool.query(query, values);
 
