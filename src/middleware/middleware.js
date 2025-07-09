90ba52c4216c2b617a4258ba9f357a4289dcc094
Jorge Diaz <jorgediazl1998@gmail.com>
2025-07-08 21:35:18 -0400

    Add middleware verifyToken

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken')
+
 const logRoute = (req, res, next) => {
   console.log(
     `[${new Date().toISOString()}] Ruta accedida: ${req.method} ${req.path}`
@@ -6,6 +9,22 @@ const logRoute = (req, res, next) => {
   next();
 };
 
+const validateToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) throw new Error("Token Missing");
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid or expired token." });
+  }
+};
+
 module.exports = {
-    logRoute
+    logRoute,
+    validateToken
 }
