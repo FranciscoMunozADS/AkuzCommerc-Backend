c6b1ca181e2a8550308acfd1823c67457a6b3cb4
Francisco Muñoz <munozjaraf@gmail.com>
2025-07-09 18:54:51 -0400

    Registrar Usuario + validaciones

---
+++ src/controllers/controllerUser.js
@@ -0,0 +1,59 @@
+const { insertUser, findUserByEmailOrId } = require('../models/queryUser');
+const bcrypt = require('bcrypt');
+
+const registerUser = async (req, res) => {
+    try {
+        const {
+            idUsuario,
+            nombre_completo,
+            telefono,
+            e_mail,
+            password,
+            url_avatar,
+        } = req.body;
+
+        // Validaciones
+
+        if (!idUsuario || !nombre_completo || !telefono || !e_mail || !password) {
+            return res.status(400).json({ error: "Faltan datos obligatiorios." });
+        }
+
+        if (!e_mail.includes("@") || !e_mail.includes(".")) {
+            return res.status(400).json({ error: "Formato de email inválido." });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ error: "La contraseña debe tener al menos 6 carácteres." });
+        }
+
+        // Verificar si el usuario ya existe
+        const exist = await findUserByEmailOrId(e_mail, idUsuario);
+        if (exist) {
+            return res.status(409).json({ error: "El usuario ya existe." }); // 409 implica conflicto
+        }
+
+        // Encriptar contraseña
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insertar el nuevo usuario
+        const nuevoUsuario = await insertUser({
+            idUsuario,
+            nombre_completo,
+            telefono,
+            e_mail,
+            password: hashedPassword,
+            url_avatar,
+        });
+
+        res.status(201).json({
+            message: "Usuario registrado exotosamente.",
+            user: nuevoUsuario,
+        });
+    }   catch (error) {
+        console.error("Error al registrar usuario:", error.message);
+        res.status(500).json({ error: "Error interno del servidor." });
+    }
+};
+
+module.exports = { registerUser };
---
+++ src/models/queryUser.js
@@ -0,0 +1,43 @@
+const pool = require("../database/db.js");
+
+/***** REGISTRO *****/
+
+// Registrar un nuevo usuario
+const insertUser = async (payload) => {
+  const { idUsuario, nombre_completo, telefono, e_mail, password, url_avatar } =
+    payload;
+
+  const query = `
+        INSERT INTO usuarios
+        (idUsuario, nombre_completo, telefono, e_mail, password, url_avatar)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING id, idUsuario, nombre_completo, telefono, e_mail, url_avatar
+    `;
+
+  const values = [
+    idUsuario,
+    nombre_completo,
+    telefono,
+    e_mail,
+    password,
+    url_avatar,
+  ];
+
+  const { rows } = await pool.query(query, values);
+  return rows[0];
+};
+
+// Validación de si existe el usuario
+
+const findUserByEmailOrId = async (e_mail, idUsuario) => {
+    const query = `
+    SELECT * FROM usuarios
+    WHERE e_mail = $1 OR idUsuario = $2
+  `;
+    const values = [e_mail, idUsuario];
+    const { rows } = await pool.query(query, values);
+    return rows[0]; // retorna el primer usuario o undefined si no existe
+};
+
+
+module.exports = { insertUser, findUserByEmailOrId };
